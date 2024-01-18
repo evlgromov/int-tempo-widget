e1a994c44874bda82c4f6a19c916f77eda858bc1
gromov <evgr89@yandex.ru>
2024-01-18 13:21:15 +0400

    init

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const mountEl = document.querySelector("#widget");
+
+createApp(App, {
+    props: ["message"]
+}, { ...mountEl.dataset }).mount('#widget')
